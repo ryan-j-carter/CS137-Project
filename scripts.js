d43f8ed393a78e8ebcf001946558b95abfeb4589
Ryan Carter <ryan.carter.j@gmail.com>
2016-04-16 19:03:58 -0700

    Added functionality for using products page to click into checkout with the correct item.
    Also finished descriptions of each item. 
    Finally, added query_param function that anyone can use to get variables from the url, such as:
    website.com/checkout.html?i=1

--- scripts.js
+++ scripts.js
@@ -37,8 +37,12 @@
  }
 
  /*
-	
+	load_changed() and set_checkout() update the checkout.html
+	page to ensure the correct item is displayed
+
+	query_param(field) parses the url query and returns the specified variable 'field'
  */
+
 var images = ["images/aeropress.jpg",  "images/encore.jpg",     "images/virtuoso.jpg", 
 			  "images/bonavita.jpg",   "images/chemex.jpg",     "images/hariokettle.jpg", 
 			  "images/kalita.jpg",     "images/v60copper.jpg",  "images/costaricalm.jpg",
@@ -48,14 +52,14 @@ var names = ["Aeropress", "Baratza Encore", "Baratza Virtuoso", "Bonavita Kettle
 			 "Kalita Wave", "V60 - Copper", "Costa Rica La Minita", "Ethiopia Halo Bariti", "Ethiopia Kochere",
 			 "Guatemala Antigua Hunapu", "Guatemala Candelaria", "Honduras Las Capucas"];
 var prices = ["$25", "$130", "$230", "$75", "$45", "$35", "$25", "$45", "$14", "$14", "$17", "$15", "$17", "$15"];
-var descriptions = ["placeholder aero",
-				    "placeholder enco",
-					"placeholder virt",
-					"placeholder bona",
-					"placeholder chem",
-					"placeholder hari",
-					"placeholder kali",
-					"placeholder v60c",
+var descriptions = ["The Aeropress is a unique brewing tool that utilizes pressure and total immersion of the grinds to extract the most possible flavor from the beans. Due to the fine filtration and shorter brewing time, the Aeropress results in a much less acidic cup of coffee. By using a smaller grind, it is possible to brew espresso-style coffee with this tool.",
+				    "The Baratza Encore is one of the best entry level burr grinders on the market. The grinder can adjust from fine espresso all the way to coarse french press, and the conical burrs ensure a uniform grind at any level. The hopper is designed to keep beans pushing against the burrs at all times, while the burrs funnel grinds through to avoid buildup.",
+					"In the mid-range of burr grinders sits the Baratza Virtuoso. With more precise burrs than the Encore, the Virtuoso offers a top level grind consistency while still being fairly low cost. The system also allows users to control the speed of the motor, letting you sacrifice some grind speed for an even better grind.",
+					"The Bonavita Kettle is one of the only electric gooseneck kettles on the market that features variable temperature control. With an accuracy of 1&deg;F, this kettle allows you to keep water at the perfect temperature for any brewing method. Other features include a timer function, and the ability to maintain a temperature for as long as you need.",
+					"The Chemex is generally regarded as one of the best and most well-designed brewing tools available. It utilizes a pour-over method in which the water continuously passes through coffee grinds until it reaches the tip of the filter. The result is a full extraction of what the beans have to offer while never extracting too much.",
+					"If you want a stovetop gooseneck kettle, the Hario is the way to go. The handle is designed to make pouring feel natural and comfortable, while the tip of the gooseneck is just the right angle to keep water flowing smoothly at any pour speed. In it's price range, there is no alternative to the Hario.",
+					"The Kalita Wave is one of the more unique pour-over tools available. Rather than funnel water to a large hole, the Kalita has three small holes in the flat base. This results in a slower extraction, and leaves the grinds immersed for a longer period of time. This will produce a much different cup than something like the V60.",
+					"If any pour-over method is considered the standard, it's the V60. Used in coffee shops everywhere, the V60 can make fantastic cups of coffee from nearly any type of bean. The copper-plated body keeps the water hot as it passes through, and looks bloody gorgeous doing it.",
 					"With a consistent cupping score of 92 and higher, La Manita is often nominated one of the best coffees in the world. It has undertones of dark chocolate, with hints of lemon and flowers behind it, and the aftertaste lingers fantastically.",
 					"This coffee is from the brand new Halo Bariti co-op south of the Yirgacheffe region. The coffee is creamy with a sweet, blueberry flavor. While typical fruit coffees end with an earthy note, this one ends with a sweet creamy flavor. Other notes include lavendar, rose, and raspberry.",
 					"Another coffee near the Yirgacheffe region, the Kochere is a light, floral bean with a tea-like aroma. Notes are reminiscent of lemon and peach tea, with some hints of oranges and melon. This is one of our lighter and more floral coffees, and makes for a great iced coffee as well as a balanced, refreshing hot cup.",
@@ -70,6 +74,20 @@ function load_changed() {
 	document.getElementById("co_item_name").innerHTML = names[index];
 	document.getElementById("co_item_price").innerHTML = prices[index];
 	document.getElementById("co_item_desc").innerHTML = descriptions[index];
+}
+
+var query_param = function(field) {
+	var href = window.location.href;
+	var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i');
+	var val = reg.exec(href);
+	return val ? val[1] : null;
+};
+
+function set_checkout() {
+	var item = document.checkout_form.product;
+	var index = query_param('i');
+	item.value = index;
+	load_changed();
 }
 
 
