f7ee79daf2ebfc0608f750dc1bea31227cb46559
Ryan Carter <ryan.carter.j@gmail.com>
2016-05-02 20:18:01 -0700

    update to calculate cost and display item name
    Also added some styling to css page

--- confirmation.php
+++ confirmation.php
@@ -22,41 +22,57 @@ require_once "pdo.php";
     </head>
     <body onload="window_onload();">
     	<div id="header">
-            <h1 id="title">Order Confirmation</h1>
+            <h1 id="title">JavaSipt</h1>
         </div>
-		<div class="products">
-			<?php
-			require_once "pdo.php";
-			$order_id = $_GET['order_id'];
-			$cost = $_GET['c'];
-			$stmt = $pdo->query("SELECT * FROM orders WHERE order_id = $order_id");
-			$row = $stmt->fetch();
-			?>
-			<h3>Your purchase was successful!</h3>
-			<h4>Shipping Information:</h4>
-			First Name: <?php echo $row['first_name']; ?><br />
-			Last Name: <?php echo $row['last_name']; ?><br />
-			Address: <?php echo $row['ship_address']; ?><br />
-			City: <?php echo $row['ship_city']; ?><br />
-			State: <?php echo $row['ship_state']; ?><br />
-			Zip Code: <?php echo $row['ship_zip']; ?>br />
-			Phone Number: <?php echo $row['ship_phone']; ?><br /><br />
-			<h4>Billing Information:</h4>
-			Address: <?php echo $row['bill_address']; ?><br />
-			City: <?php echo $row['bill_city']; ?><br />
-			State: <?php echo $row['bill_state']; ?><br />
-			Zip Code: <?php echo $row['bill_zip']; ?><br />
-			Phone Number: <?php echo $row['bill_phone']; ?><br /><br />
-			<h4>Payment Method: <?php echo $row['payment_method']; ?></h4>
-			<h4>Card Information:</h4>
-			Card Number: ************<?php echo substr(strval($row['payment_method']),12); ?><br />
-			Name on Card: <?php echo $row['card_name']; ?><br />
-			Expiration Date: <?php echo $row['card_expiration_month']; ?> <?php echo $row['card_expiratin+year']; ?><br /><br />
-			<h4>Order Details:</h4>	
-			Item Purchased: <?php echo $row['product_name']; ?><br />
-			Quantity: <?php echo $row['quantity']; ?><br />
-			Total Cost: $<?php echo $cost; ?><br /><br />
-			<h3 align="center"><a href="index.html"">Return to Home</a></h3>
-		</div>
+	<div id="nav" class="nav_rel">
+            <ul>
+                <li><a href="index.html">Home</a></li><!-- display inline block :(
+             --><li><a href="products.php">Products</a></li>
+            </ul>
+        </div>
+	<div id="firstcontent" class="content">
+		<?php
+		require_once "pdo.php";
+		$order_id = $_GET['order_id'];
+		$stmt = $pdo->query("SELECT o.*, p.price FROM orders o INNER JOIN product p ON o.product_name=p.name WHERE o.order_id = $order_id");
+		$row = $stmt->fetch();
+		?>
+		<h2>Order Receipt<div class="hr_div"></div></h2>
+		<h4>Product Information:</h4>
+		<p><?php echo $row['product_name']." - $".$row['price'];  ?></p>
+		<p>Quantity: <?php echo $row['quantity'];  ?><br>
+		<?php if ($row['ship_method'] == 6){
+			$ship = "6-Days Ground - $3";
+			$ship_price = 3;
+		}
+		else if ($row['ship_method'] == 2) {
+			$ship = "2-Days Expedited - $6";
+			$ship_price = 6;
+		} 
+		else {
+			$ship = "Overnight - $10";
+			$ship_price = 10;
+		}
+		$subtotal = $row['price']*$row['quantity'] + $ship_price;
+		?>
+		Shipping Speed: <?php echo $ship;  ?><br>
+		Subtotal: $<?php echo $subtotal;  ?><br>
+		Card Number: xxxxxxxxxxxx<?php echo substr($row['card_number'], 12, 4);  ?><br>
+		Card Type: <?php echo $row['payment_method'];  ?></p>
+		<h4>Shipping Information:</h4>
+		<p>Name: <?php echo $row['first_name']." ".$row['last_name']; ?><br>
+		Address: <?php echo $row['ship_address']; ?><br>
+		City: <?php echo $row['ship_city']; ?><br>
+		State: <?php echo $row['ship_state']; ?><br>
+		Zip Code: <?php echo $row['ship_zip']; ?><br>
+		Phone Number: <?php echo $row['ship_phone']; ?></p>
+		<h4>Billing Information:</h4>
+		<p>Address: <?php echo $row['bill_address']; ?><br>
+		City: <?php echo $row['bill_city']; ?><br>
+		State: <?php echo $row['bill_state']; ?><br>
+		Zip Code: <?php echo $row['bill_zip']; ?><br>
+		Phone Number: <?php echo $row['bill_phone']; ?></p>
+	</div>
+	<div style="height:50px;width:100%;"></div>
     </body>
 </html>
--- scripts.js
+++ scripts.js
@@ -176,16 +176,14 @@ function submit_order() {
 	var frm = document.checkout_form;
 	var xhttp = new XMLHttpRequest();
 	var item_name = document.getElementById("co_item_name").innerHTML;
-	var item_price = document.getElementById("co_item_price").innerHTML;
-	
+
 	var order_num = Math.floor(Math.random() * 1000000000);
-	var cost = parseInt(frm.quantity.value)*parseInt(item_price.substring(1));
-	cost += parseInt(frm.shipping_method.value);
+	
 	xhttp.onreadystatechange = function() {
 		if(xhttp.readyState == 4 && xhttp.status == 200){
 			alert("Submission Complete.");
 			// opens confirmation page with randomly generated order number
-			window.location = "confirmation.php?order_id=" + order_num + "&c=" + cost;
+			window.location = "confirmation.php?order_id=" + order_num;
 		}
 	}
 
@@ -210,7 +208,7 @@ function submit_order() {
 			"&card_expiration_month="+frm.month.value+
 			"&card_expiration_year="+frm.year.value+
 			"&card_security="+frm.securitycode.value+
-			"&ship_method="+frm.shipping_method.value+
+			"&ship_method="+frm.shipping_method.options[frm.shipping_method.selectedIndex].text+
 			"&order_id="+order_num
 		);
 }
